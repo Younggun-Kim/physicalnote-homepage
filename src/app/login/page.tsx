685ea0ede1f04f776ded76eb122b8cf3f17ed59b
Conner <dudrjs145@naver.com>
2024-12-25 02:56:12 +0900

    feat: 기본 파일 주소 생성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import useLoginStore from '@/store/loginStore';
+
+export default function LoginPage() {
+  const { resetState } = useLoginStore((state) => state.actions);
+
+  useEffect(() => {
+    resetState();
+  }, [resetState]);
+
+  return (
+    <div className="flex justify-center items-start w-[100vw]">
+      <div className="fixed w-full min-h-[734px] top-0 bottom-[20%] z-0">
+        <Image
+          src="/images/loginBackground.svg"
+          alt=""
+          fill
+        />
+      </div>
+      <div className="relative w-full flex flex-col items-center z-10">
+        {/*<LoginTitle>로그인</LoginTitle>*/}
+        {/*<LoginForm />*/}
+        {/*<LoginSubmit />*/}
+      </div>
+    </div>
+  );
+}
