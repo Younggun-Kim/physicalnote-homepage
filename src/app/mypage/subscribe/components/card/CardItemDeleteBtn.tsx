d796342b0b7310b625a515844e3246b79537fd59
Conner <dudrjs145@naver.com>
2024-12-26 07:17:38 +0900

    feat: 카드 삭제 기능 추가

--- src/app/mypage/subscribe/components/card/CardItemDeleteBtn.tsx
+++ src/app/mypage/subscribe/components/card/CardItemDeleteBtn.tsx
@@ -2,19 +2,19 @@
 
 // import useDeleteBillingKey from '@/api/query/payment/useDeleteBillingKeys';
 import GetBillingKeysResponseDto from '@/api/dto/payment/GetBillingKeysResponseDto';
+import useDeleteBillingKey from '@/app/utils/query/payment/useDeleteBillingKey';
 
 interface Props {
   billingKey: GetBillingKeysResponseDto;
 }
 
 export default function CardItemDeleteBtn({ billingKey }: Props) {
-  // TODO: 카드 삭제 API
-  // const deleteMutation = useDeleteBillingKey();
+  const deleteMutation = useDeleteBillingKey();
   const handleClick = async () => {
     if (!confirm('등록하신 카드를 삭제하시겠습니까?')) return;
     try {
       if (!billingKey) return;
-      // await deleteMutation.mutateAsync(billingKey.customerKey);
+      await deleteMutation.mutateAsync(billingKey.customerKey);
     } catch (e) {
       console.log(e);
     }
