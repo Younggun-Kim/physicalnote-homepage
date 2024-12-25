3546d2c69f1df96c04c87630f4d40304b86fb5d0
Conner <dudrjs145@naver.com>
2024-12-26 07:20:12 +0900

    feat: 수단 변경 기능 추가

--- src/app/mypage/subscribe/components/card/CardItemChangeBtn.tsx
+++ src/app/mypage/subscribe/components/card/CardItemChangeBtn.tsx
@@ -1,20 +1,20 @@
 'use client';
 // import usePutBillingKeysDefault from '@/api/query/payment/usePutBillingKeysDefault';
 import GetBillingKeysResponseDto from '@/api/dto/payment/GetBillingKeysResponseDto';
+import usePutBillingKeysDefault from '@/app/utils/query/payment/usePutBillingKeysDefault';
 
 interface Props {
   billingKey: GetBillingKeysResponseDto;
 }
 
 export default function CardItemChangeBtn({ billingKey }: Props) {
-  // TODO: 수단 변경 API
-  // const defaultMutation = usePutBillingKeysDefault();
-
+  // TODO: 수단 변경 테스트 필요
+  const defaultMutation = usePutBillingKeysDefault();
   const handleClick = async () => {
     if (!confirm('결제 수단을 변경하시겠습니까?')) return;
     try {
       console.log(billingKey);
-      // await defaultMutation.mutateAsync(billingKey.customerKey);
+      await defaultMutation.mutateAsync(billingKey.customerKey);
     } catch (e) {}
   };
 
