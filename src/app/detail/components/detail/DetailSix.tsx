62194efc0c2cdca17ec2f053ff26fed5d5334db1
Conner <dudrjs145@naver.com>
2025-01-01 19:31:12 +0900

    feat: 상세내용 6번째 이미지 변경

--- src/app/detail/components/detail/DetailSix.tsx
+++ src/app/detail/components/detail/DetailSix.tsx
@@ -1,44 +1,41 @@
-'use client';
-import styled from 'styled-components';
-
 export default function DetailSix() {
-  return <StyledContainer></StyledContainer>;
+  return (
+    <div className="w-full">
+      <picture className="block w-full h-full object-cover">
+        <source
+          media="(min-width: 1921px)"
+          srcSet="/images/detail/detail_6_2560.svg"
+        />
+
+        <source
+          media="(min-width: 1441px) and (max-width: 1920px)"
+          srcSet="/images/detail/detail_6_1920.svg"
+        />
+
+        <source
+          media="(min-width: 1025px) and (max-width: 1440px)"
+          srcSet="/images/detail/detail_6_1440.svg"
+        />
+
+        <source
+          media="(min-width: 769px) and (max-width: 1024px)"
+          srcSet="/images/detail/detail_6_1024.svg"
+        />
+
+        <source
+          media="(min-width: 426px) and (max-width: 768px)"
+          srcSet="/images/detail/detail_6_768.svg"
+        />
+        <source
+          media="(min-width: 321px) and (max-width: 425px)"
+          srcSet="/images/detail/detail_6_425.svg"
+        />
+
+        <img
+          src="/images/detail/detail_6_320.svg"
+          alt=""
+        />
+      </picture>
+    </div>
+  );
 }
-
-const StyledContainer = styled.img`
-  width: 100%;
-  height: 1260px;
-  margin: 0 auto;
-  content: url('/images/detail/detail_3_320.png');
-
-  @media (min-width: 1921px) {
-    max-width: 2560px;
-    height: 1080px;
-    content: url('/images/detail/detail_3_2560.png');
-  }
-
-  @media (min-width: 1441px) and (max-width: 1920px) {
-    height: 1080px;
-    content: url('/images/detail/detail_3_1920.png');
-  }
-
-  @media (min-width: 1025px) and (max-width: 1440px) {
-    height: 1415px;
-    content: url('/images/detail/detail_3_1440.png');
-  }
-
-  @media (min-width: 769px) and (max-width: 1024px) {
-    height: 1160px;
-    content: url('/images/detail/detail_3_1024.png');
-  }
-
-  @media (min-width: 426px) and (max-width: 768px) {
-    height: 1270px;
-    content: url('/images/detail/detail_3_768.png');
-  }
-
-  @media (min-width: 321px) and (max-width: 425px) {
-    height: 1340px;
-    content: url('/images/detail/detail_3_425.png');
-  }
-`;
