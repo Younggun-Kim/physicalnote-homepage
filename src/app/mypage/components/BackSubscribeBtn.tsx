c00369015a86a19405a4f76a9a589bd3f7cefc04
Conner <dudrjs145@naver.com>
2025-01-16 02:21:43 +0900

    feat: 카드 등록 실패 후 뒤로가기로 변경

--- src/app/mypage/components/BackSubscribeBtn.tsx
+++ src/app/mypage/components/BackSubscribeBtn.tsx
@@ -1,14 +1,16 @@
 'use client';
-import Link from 'next/link';
+
+import { useRouter } from 'next/navigation';
 
 export const BackSubscribeBtn = () => {
+  const router = useRouter();
   return (
-    <Link
-      href={'/mypage/subscribe'}
+    <button
+      onClick={() => router.back()}
       className="rounded-full text-gray1 font-sans text-sm md:text-base lg:text-xl font-bold bg-secondary flex justify-center items-center px-[26px] py-[9px] cursor-pointer hover:cursor-pointer"
     >
       뒤로가기
-    </Link>
+    </button>
   );
 };
 
